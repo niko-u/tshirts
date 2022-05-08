4c96584c076e459b86b105689f81df738ba1b813
niko-u <77936525+niko-u@users.noreply.github.com>
2022-05-07 21:50:36 -0700

    Navbar and submissions

---
+++ src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+
+<nav class="navbar fixed-top navbar-expand-lg navbar-light " style="background-color: #89cc95;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#" id="title">TSHIRTS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Artist Portal</a>
+        </li>
+        <li class="nav-item">
+
+        </li>
+      </ul>
+            <form class="d-flex">
+                <input id="search" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            </form>
+        <a class="navbar-brand" href="#">
+            <img src="../assets/profilepic.png" alt="" width="35" height="35" class="rounded-circle d-inline-block align-text-top" id="profile">
+        </a>
+    </div>
+  </div>
+</nav>
+
+    
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+.nav-link {
+    font-size: 20px;
+    font-family: Arial;
+    font-style: bold;
+}
+
+#profile {
+    margin-left: 20px;
+}
+
+#title {
+    font-size: 25px;
+    font-family: Arial;
+    font-style: bold;
+}
+
+
+</style>
---
+++ src/components/Submission.vue
@@ -0,0 +1,70 @@
+<template>
+
+ <div class="container bcontent">
+        <div class="card" style="width: 800px;">
+            <div class="row no-gutters">
+                <div class="col-sm-5">
+                    <img class="card-img" src="../assets/shirt.jpeg" alt="tshirt">
+                </div>
+                <div class="col-sm-7">
+                    <div class="card-body">
+                        <h5 class="card-title"> {{ title }} Design Title | <a class="artist" href="">@artist {{ artist }}</a></h5>
+                            <p class="card-text"> {{ description }} This tshirt was designed to be the best quality design possible</p>
+                        <hr>
+                            <button type="button" class="btn btn-outline-success"> 9999 {{ votes }} </button>
+
+                        <div id="bottom">
+                            <p id="tags">tags: </p>
+                            <div v-for="tag in tags">
+                                <a href="tag.link"> {{ tag.title }} </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+</template>
+
+<script>
+
+export default {
+    data() {
+        
+    }
+}
+
+</script>
+
+<style scoped>
+
+.artist {
+    text-decoration: none;
+    color: #89cc95;
+}
+
+#tags {
+    float: left;
+}
+
+#bottom {
+    position: absolute;
+    bottom: 0px;
+}
+
+#left {
+    float: right;
+}
+
+#right {
+    float: right;
+}
+
+
+
+</style>
